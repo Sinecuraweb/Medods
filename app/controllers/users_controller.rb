3d05338813ff51e07d51b349b822d37926053a4e
Egor Tregubenko <sinecura92@mail.ru>
2019-01-29 17:18:18 +0500

    fix deprecated before_filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
-	before_filter :authenticate_user! # для Devise
+	#before_filter :authenticate_user! # для Devise (устарело в rails 5.1)
+	before_action :authenticate_user! # для Devise
 
 	def index
 	  @users = User.all
