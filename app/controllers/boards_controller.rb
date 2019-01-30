514a7e37f363a89e1864ab701954bb2ad82dab5e
Egor Tregubenko <sinecura92@mail.ru>
2019-01-30 14:33:29 +0500

    fix more

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,40 +1,28 @@
 class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
-
   # GET /boards.json
   def index
     #@boards = Board.all 
     @boards = Board.search(params[:search])
   end
-
   # GET /boards/1
   # GET /boards/1.json
   def show
   end
-
   # GET /boards/new
   def new
     @board = Board.new
-
     #Свяжем авторизованного юзера и новое объявление
     #@users = User.all
     @user_options = User.all.map{|u| [ u.name, u.id ] }
-
   end
-
   # GET /boards/1/edit
   def edit
   end
-
-
   # POST /boards.json
-
   def create
-
     @board = Board.new(board_params)
-
     @board.user = current_user
-
     respond_to do |format|
       if @board.save
         format.html { redirect_to @board, notice: 'Ваше объявление успешно добавлено на сайт!' }
@@ -45,7 +33,6 @@ class BoardsController < ApplicationController
       end
     end
   end
-
   #/boards/1.json
   def update
     respond_to do |format|
@@ -58,9 +45,6 @@ class BoardsController < ApplicationController
       end
     end
   end
-
-
-
   def destroy
     @board.destroy
     respond_to do |format|
@@ -68,13 +52,11 @@ class BoardsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_board
       @board = Board.find(params[:id])
     end
-
     #All params
     def board_params
       params.require(:board).permit(:title, :body, :picture, :all_tags, :author)
