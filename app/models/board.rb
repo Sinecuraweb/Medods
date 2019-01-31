1181b577b702572ea0d91c8bf68983e2a4655424
Egor Tregubenko <sinecura92@mail.ru>
2019-01-31 10:03:12 +0500

    fix

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,36 +1,30 @@
-lass Board < ApplicationRecord
-	mount_uploader :picture, PictureUploader
-	has_many :comments
+# Board Class
+class Board < ApplicationRecord
+  mount_uploader :picture, PictureUploader
+  has_many :comments
+  # Has_belong_to_many
+  has_many :taggings
+  has_many :tags, through: :taggings
+  belongs_to :user
+  #start search
+  def self.search(search)
+    if search
+      where(["title LIKE ?", "%#{search}%"])
+    else
+      all
+    end
+  end
 
-	#has_belong_to_many
-	has_many :taggings
-	has_many :tags, through: :taggings
-	
-	belongs_to :user
+  #start tag
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
 
-
-	#start search
-	def self.search(search)
-		if search
-			where(["title LIKE ?", "%#{search}%"])
-
-		else
-			all
-		end
-	end
-
-	#start tag
-
-
-	def all_tags
-		self.tags.map(&:name).join(', ')
-	end
-
-	def all_tags=(names)
-		#Добавляем теги к посту
-		self.tags = names.split(',').map do |name|
-			#Ищеи тег по имени
-			Tag.where(name: name.strip).first_or_create!
-		end
-	end
+  def all_tags=(names)
+    #Добавляем теги к посту
+    self.tags = names.split(',').map do |name|
+    #Ищеи тег по имени
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 end
